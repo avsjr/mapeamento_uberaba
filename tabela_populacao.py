097357612910153dfe7f545c2f670a1d7ba9cb85
Antonio Vicente <avsj@me.com>
2024-02-21 17:02:55 -0300

    ajustes tabelas

--- tabela_populacao.py
+++ tabela_populacao.py
@@ -4,27 +4,27 @@ import pandas as pd
 # Carregar os dados dos bairros
 dados_bairros = pd.read_excel('c:/Users/avsj/iCloudDrive/01.Antonio/01.Projetos/01.github/mapeamento_uberaba/files/03.IBGE/UPG_UBERABA.xlsx')
 
-#POPULACAO_TOTAL = dados_bairros['POPULACAO_TOTAL']
+# Definir o tamanho da fonte para o cabeçalho
+font_size_header = "16px"
 
 # Criar a tabela interativa
 fig = go.Figure(data=[go.Table(
-    header=dict(values=['Bairro', 'População Total &#9794;&#9792;', 'Homens &#9794;', 'Mulheres &#9792;']),
+    header=dict(values=[
+        f'<span style="font-size:{font_size_header};">Bairro</span>',
+        f'<span style="font-size:{font_size_header};">Total 🚹🚺</span>',
+        f'<span style="font-size:{font_size_header};">Homens 🚹</span>',
+        f'<span style="font-size:{font_size_header};">Mulheres 🚺</span>'
+    ]),
     cells=dict(values=[
         [bairro for bairro in dados_bairros['UPG']],
-        [f"{populacao} &#9794;&#9792;" for populacao in dados_bairros['POPULACAO_TOTAL']],
-        [f"{homens} &#9794;" for homens in dados_bairros['HOMENS']],
-        [f"{mulheres} &#9792;" for mulheres in dados_bairros['MULHERES']]
+        [f"{populacao} " for populacao in dados_bairros['POPULACAO_TOTAL']],
+        [f"{homens} " for homens in dados_bairros['HOMENS']],
+        [f"{mulheres} " for mulheres in dados_bairros['MULHERES']]
     ])
 )])
 
 # Adicionar título
-fig.update_layout(title='Dados Populacionais por Bairro')
-
-# Exibir a tabela interativa
-#fig.show()
-
-# Salvar como imagem (PNG)
-#fig.write_image("tabela_populacional.png")
+fig.update_layout(title='População por Bairro')
 
 # Salvar como HTML
 fig.write_html("tabela_populacional.html")
